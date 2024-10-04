272438bddc8e4218fa579c908ad12c9eb7b68610
szymurmur <szymo0215@gmail.com>
2024-10-04 09:26:24 +0200

    add Users database model

--- database/models.py
+++ database/models.py
@@ -4,11 +4,22 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 class Base(DeclarativeBase):
     pass
 
+class Users(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key = True)
+    email: Mapped[str] = mapped_column(String(50))
+    priority_group_id: Mapped[int] = mapped_column(ForeignKey("priority_groups.id"))
+
+    priority_groups: Mapped["PriorityGroups"] = relationship(back_populates = "users")
+
 class PriorityGroups(Base):
     __tablename__ = "priority_groups"
 
     id: Mapped[int] = mapped_column(primary_key = True)
     priority: Mapped[int]
+
+    users: Mapped["Users"] = relationship(back_populates = "priority_groups")
 
 class Statuses(Base):
     __tablename__ = "statuses"
