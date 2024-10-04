ff1ef2468f76bf3733a0f7a3b8438fe7c2d0e38d
szymurmur <szymo0215@gmail.com>
2024-10-04 09:15:56 +0200

    add PriorityGroups database model

--- database/models.py
+++ database/models.py
@@ -4,6 +4,12 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 class Base(DeclarativeBase):
     pass
 
+class PriorityGroups(Base):
+    __tablename__ = "priority_groups"
+
+    id: Mapped[int] = mapped_column(primary_key = True)
+    priority: Mapped[int]
+
 class Statuses(Base):
     __tablename__ = "statuses"
 
