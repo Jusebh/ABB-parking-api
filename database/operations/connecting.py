688c20f0f00e848eb3ed1d953cf08355d12aefa1
szymurmur <szymo0215@gmail.com>
2024-10-07 12:53:26 +0200

    fix database imports

--- database/operations/connecting.py
+++ database/operations/connecting.py
@@ -3,5 +3,5 @@ from sqlalchemy import create_engine
 
 def connect_to_database():
     # connection_string = os.getenv("CONN_STRING")
-    connection_string = "sqlite:///"
+    connection_string = "sqlite:///test.db"
     return create_engine(connection_string, echo = True)
