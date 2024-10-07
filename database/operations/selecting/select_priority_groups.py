c1ef23c36cd0be62b0e6be06bf44efeea4cf1c39
Jusebh <ddmularski@gmail.com>
2024-10-07 10:45:39 +0200

    add functions to select data from database

---
+++ database/operations/selecting/select_priority_groups.py
@@ -0,0 +1,9 @@
+from database.operations.connecting import connect_to_database
+from database.models import PriorityGroups
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+def select_user():
+    with Session(connect_to_database()) as session:
+        stmt = select(PriorityGroups)
+        return session.scalars(stmt).all()
