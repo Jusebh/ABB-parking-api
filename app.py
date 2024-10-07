bc35f7e66db8d8aeca4177339af0feb51bab1954
szymurmur <szymo0215@gmail.com>
2024-10-07 13:09:11 +0200

    add admin routes for getting tables

--- app.py
+++ app.py
@@ -1,10 +1,53 @@
 from flask import Flask
+from database.operations.create_tables import create_tables
+from database.operations.selecting.select_all_users import select_all_users
+from database.operations.selecting.select_all_priority_groups import select_all_priority_groups
+from database.operations.selecting.select_all_reservations import select_all_reservations
+from database.operations.selecting.select_all_reservations_dates import select_all_reservations_dates
+from database.operations.selecting.select_all_statuses import select_all_statuses
 
-app = Flask(__name__)
+app = Flask(__name__) 
+create_tables()
 
-@app.route("/")
-def hello_world():
-    return "Hello World!"
+@app.route("/admin/get/allUsers")
+def get_all_users():
+    users = select_all_users()
+    users_tab = []
+    for user in users:
+        users_tab.append({"id": user.id, "email": user.email, "priority_group_id": user.priority_group_id})
+    return users_tab
 
-if __name__ == '__main__':  
-   app.run(debug=True) 
+@app.route("/admin/get/allPriorityGroups")
+def get_all_priority_groups():
+    priority_groups = select_all_priority_groups()
+    priority_groups_tab = []
+    for priority_group in priority_groups:
+        priority_groups_tab.append({"id": priority_group.id, "priority": priority_group.priority})
+    return priority_groups_tab
+
+@app.route("/admin/get/allReservations")
+def get_all_reservations():
+    reservations = select_all_reservations()
+    reservations_tab = []
+    for reservation in reservations:
+        reservations_tab.append({"id": reservation.id, "user_id": reservation.user_id, "status_id": reservation.status_id, "created_at": reservation.created_at})
+    return reservations_tab
+
+@app.route("/admin/get/allReservationsDates")
+def get_all_reservations_dates():
+    reservations_dates = select_all_reservations_dates()
+    reservations_dates_tab = []
+    for reservations_date in reservations_dates:
+        reservations_dates_tab.append[{"id": reservations_date.id, "reservation_id": reservations_date.reservation_id, "date_of_reservation": reservations_date.date_of_reservation}]
+    return reservations_dates_tab
+
+@app.route("/admin/get/allStatuses")
+def get_all_statuses():
+    statuses = select_all_statuses()
+    statuses_tab = []
+    for status in statuses:
+        statuses_tab.append({"id": status.id, "title": status.title})
+    return statuses_tab
+
+if __name__ == '__main__':
+   app.run(debug=True)
