9a60cbe3f0df765666b3ff078eb59293657ca13c
szymurmur <szymo0215@gmail.com>
2024-10-08 13:12:14 +0200

    split removing into separate functions

---
+++ database/operations/removing/remove_user.py
@@ -0,0 +1,14 @@
+from database.operations.connecting import connect_to_database
+from database.models import Users
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+def remove_record(id):
+    with Session(connect_to_database()) as session:
+        stmt = select(Users).where(Users.id == id)
+        result = session.scalars(stmt).one_or_none()
+        if result:
+            session.delete(result)
+            session.commit()
+            return True
+        return False
