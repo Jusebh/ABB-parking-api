78dec80cd94414a2e043bd8263cc370bf2d984b5
Jusebh <ddmularski@gmail.com>
2024-10-08 09:12:57 +0200

    add registering of all blueprints

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from database.operations.create_tables import create_tables
 from blueprints.admin.get import get_all_priority_groups, get_all_reservations, get_all_reservations_dates, get_all_statuses, get_all_users
+from blueprints.user.dates import current_month, table_of_days
+from blueprints.user.get import get_reservation_by_date
 
 app = Flask(__name__)
 
@@ -11,6 +13,10 @@ app.register_blueprint(get_all_priority_groups)
 app.register_blueprint(get_all_reservations)
 app.register_blueprint(get_all_reservations_dates)
 app.register_blueprint(get_all_statuses)
+
+app.register_blueprint(current_month)
+app.register_blueprint(table_of_days)
+app.register_blueprint(get_reservation_by_date)
 
 if __name__ == '__main__':
    app.run(debug=True)
