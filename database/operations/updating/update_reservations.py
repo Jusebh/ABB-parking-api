e1c06fc81cd1670eca4440308cde5c1a3007f0c0
Jusebh <ddmularski@gmail.com>
2024-10-11 11:58:00 +0200

    minor fix to update functions

--- database/operations/updating/update_reservations.py
+++ database/operations/updating/update_reservations.py
@@ -3,9 +3,9 @@ from sqlalchemy import update
 from sqlalchemy.orm import Session
 from database.models import Reservations
 
-def update_reservation(reservation_id, reservation_created_at, reservation_user_id):
+def update_reservation(reservation_id, reservation_user_id):
     with Session(connect_to_database()) as session:
-        stmt = update(Reservations).where(Reservations.id == reservation_id).values(user_id = reservation_user_id, created_at = reservation_created_at)
+        stmt = update(Reservations).where(Reservations.id == reservation_id).values(user_id = reservation_user_id)
         try: 
             session.execute(stmt)
             session.commit()
--- database/operations/updating/update_reservations_dates.py
+++ database/operations/updating/update_reservations_dates.py
@@ -2,8 +2,10 @@ from database.operations.connecting import connect_to_database
 from sqlalchemy import update
 from sqlalchemy.orm import Session
 from database.models import ReservationsDates
+from datetime import datetime
 
 def update_reservations_date(reservation_date_id, reservation_date_reservation_id, reservation_date_status_id, reservation_date_date_of_reservation):
+    reservation_date_date_of_reservation = datetime.strptime(reservation_date_date_of_reservation, "%Y-%m-%d").date()
     with Session(connect_to_database()) as session:
         stmt = update(ReservationsDates).where(ReservationsDates.id == reservation_date_id).values(reservation_id = reservation_date_reservation_id, date_of_reservation = reservation_date_date_of_reservation, status_id = reservation_date_status_id)
         try: 
