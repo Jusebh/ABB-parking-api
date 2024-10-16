ad6643d43e1b72884000d4d886c7f632c92f55f7
Jusebh <ddmularski@gmail.com>
2024-10-16 09:56:07 +0200

    add function no notify user when status is changed

--- database/check_reservation_possibility.py
+++ database/check_reservation_possibility.py
@@ -1,14 +1,17 @@
 import calendar
 from datetime import datetime
 from database.operations.adding.add_reservation import add_reservation
+from communication.new_reservation_mail import new_reservation_mail
 from database.select_confing_data import select_config_data
 from database.operations.selecting.select_count_of_reservations import select_count_of_reservations
 from database.operations.selecting.select_priority_group import select_priority_group
 from database.operations.selecting.select_reservations_by_priority import select_reservation_by_priority
-from database.operations.updating.update_reservation_status import update_reservation_status
+from database.operations.selecting.select_user_email import select_user_email
 from database.operations.selecting.select_user_reservations_by_month import select_user_reservations_by_month
+from database.operations.updating.update_reservation_status import update_reservation_status
 
 def check_reservation_possibility(day: str, month: str, user_id, dates: list):
+    email = select_user_email(int(user_id))
     number_of_parking_spots = int(select_config_data("parking_spots_number"))
     result = []
     current_year = datetime.now().year  
@@ -32,6 +35,7 @@ def check_reservation_possibility(day: str, month: str, user_id, dates: list):
                     reservation_to_replace = select_reservation_by_priority(i, month, priority)
                     update_reservation_status(reservation_to_replace, "Rejected")
         result += add_reservation(int(user_id), day, month, dates, priority)
+        new_reservation_mail(email, dates)
     elif priority == 2:
         new_dates = dates.copy()
         for i in dates:
@@ -44,6 +48,7 @@ def check_reservation_possibility(day: str, month: str, user_id, dates: list):
                     else:
                         new_dates.remove(i)
         result += add_reservation(int(user_id), day, month, new_dates, priority)
+        new_reservation_mail(email, new_dates)
     elif priority == 3:
         new_dates = dates.copy()
         for i in dates:
@@ -52,4 +57,5 @@ def check_reservation_possibility(day: str, month: str, user_id, dates: list):
                 if number_of_reservations == number_of_parking_spots:
                     new_dates.remove(i)
         result += add_reservation(int(user_id), day, month, new_dates, priority)
+        new_reservation_mail(email, new_dates)
     return result            
---
+++ database/operations/selecting/select_email_by_reservation_id.py
@@ -0,0 +1,13 @@
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+from database.models import Reservations, ReservationsDates, Users
+from database.operations.connecting import connect_to_database
+
+def select_email_by_reservation_id(reservation_id):
+    with Session(connect_to_database()) as session:
+        stmt = select(Users).join(Users.reservations).join(Reservations.reservations_dates).where(ReservationsDates.id == reservation_id)
+        result = session.scalars(stmt).one_or_none()
+        if result:
+            return result.email
+        else:
+            return None
