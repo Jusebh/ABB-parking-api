deed531902d4627f851ed37ec1cff732da15e3b8
Jusebh <ddmularski@gmail.com>
2024-10-16 08:32:27 +0200

    add notification when making new reservation

--- communication/check_reservations.py
+++ communication/check_reservations.py
@@ -1,7 +1,7 @@
 from database.operations.selecting.select_reservation_to_notify import select_reservation_to_notify
-from communication.mail import mail
+from communication.notify_mail import notify_mail
 
 def check_reservations():
     users_to_notify = select_reservation_to_notify()
     for user in users_to_notify:
-        mail(user["email"], user["date"])
+        notify_mail(user["email"], user["date"])
---
+++ communication/new_reservation_mail.py
@@ -0,0 +1,32 @@
+import os
+from azure.communication.email import EmailClient
+
+connection_string = os.getenv("EMAIL_CONN_STRING")
+
+def new_reservation_mail(email_adress, days: list):
+    email_client = EmailClient.from_connection_string(os.getenv("EMAIL_CONN_STRING"))
+    message = {
+        "content":{
+            "subject": "Informacja dotycząca twoich nowych rezerwacji.",
+            "html": f"""  <div style="background-color: #f6f6f6">
+    <h1 style="background-color: #d9d9d9; color: rgb(95, 90, 90)">Witaj!</h1>
+    <p>
+      Twoje rezerwacje na dni {",".join(days)}, zostały przyjęte w systemie.
+    </p>
+    <p>
+      Oczekuj dalszych informacji na temat ich statusów.
+    </p>
+  </div>"""
+        },
+        "recipients":{
+            "to": [
+                {
+                    "address": email_adress
+                }
+            ]
+        },
+        "senderAddress": os.getenv("AZURE_MAIL")
+    }
+    
+    poller = email_client.begin_send(message)
+    print(poller.result())
