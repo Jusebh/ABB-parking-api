4ee9edd760c7fde4453ed8abef6991cb885261e4
Jusebh <ddmularski@gmail.com>
2024-10-16 09:13:21 +0200

    fix functions that use date in 'where' statements

--- database/operations/selecting/select_reservation_by_date.py
+++ database/operations/selecting/select_reservation_by_date.py
@@ -1,14 +1,25 @@
+import calendar
 from datetime import datetime
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 from database.models import Reservations, ReservationsDates
 from database.operations.connecting import connect_to_database
+from database.select_confing_data import select_config_data
 
 def select_reservation_by_date(user_id, day, month):
     with Session(connect_to_database()) as session:
         try:
-            date = f"{day}-{month}-{str(datetime.now().year)}"
-            date = datetime.strptime(date, "%d-%m-%Y").date()
+            next_month_open_hour = int(select_config_data("next_month_opening_hour"))
+            current_hour = datetime.now().hour
+            current_day = datetime.now().day
+            current_year = datetime.now().year
+            current_month = datetime.now().month
+            last_day = calendar.monthrange(current_year, int(month))[1]
+            
+            if int(current_day) == last_day and current_hour >= next_month_open_hour and current_month == 12:
+                current_year += 1
+            
+            date = f"{day}-{month}-{current_year}"
             stmt = select(ReservationsDates).join(ReservationsDates.reservations).where(Reservations.user_id == user_id).where(ReservationsDates.date_of_reservation >= date)
             result = session.scalars(stmt).all()
             reservations_tab = []
@@ -17,3 +28,7 @@ def select_reservation_by_date(user_id, day, month):
             return reservations_tab
         except:
             return None
+        
+        
+        
+        
