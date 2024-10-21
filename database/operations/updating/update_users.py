764abf1c1614747baaf193c4adbac13c32e525f1
Jusebh <ddmularski@gmail.com>
2024-10-21 11:58:13 +0200

    fix updates

--- database/operations/updating/update_users.py
+++ database/operations/updating/update_users.py
@@ -3,9 +3,9 @@ from sqlalchemy.orm import Session
 from database.operations.connecting import connect_to_database
 from database.models import Users
 
-def update_user(user_id, user_email):
+def update_user(user_id, user_email, priority_group_id, notifications, role):
     with Session(connect_to_database()) as session:
-        stmt = update(Users).where(Users.id == user_id).values(email = user_email)
+        stmt = update(Users).where(Users.id == user_id).values(email = user_email, priority_group_id = int(priority_group_id), notifications = notifications, role = role)
         try: 
             session.execute(stmt)
             session.commit()
