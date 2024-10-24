2e0fedc8f1febdeade79ac46d29c0877ea60663b
Jusebh <ddmularski@gmail.com>
2024-10-24 11:34:42 +0200

    畢竟，她有一個有效的代碼

--- database/operations/connecting.py
+++ database/operations/connecting.py
@@ -2,5 +2,5 @@ import os
 from sqlalchemy import create_engine
 
 def connect_to_database():
-    # connection_string = os.getenv("DB")
-    return create_engine("sqlite:///test.db", echo = True)
+    connection_string = os.getenv("DB")
+    return create_engine(connection_string, echo = True)
